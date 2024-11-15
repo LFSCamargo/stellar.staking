4cd89b85b9618d47a9fd1c6080720849432d1c7c
Luiz Fernando Sousa Camargo <luizepauloxd@gmail.com>
2024-11-15 15:07:41 -0300

    fix: fixes stellar installation script

--- script/install_stellar.sh
+++ script/install_stellar.sh
@@ -1,7 +1,7 @@
 # checks if stellar is installed using which
 if ! which stellar >/dev/null; then
   # installs stellar
-  cargo install stellar --locked
+  cargo install stellar-cli --locked
 else 
   echo "Stellar and Soroban already installed"
 fi
